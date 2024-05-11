3c50e630d3653c0f71f378e13084a69a216849b1
RitoCarlHave <carlhaverito.10@gmail.com>
2024-05-11 11:47:35 +0800

    Initial Code

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/sign_up.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Saving Tracker',
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
+      debugShowCheckedModeBanner: false,
+      home: MyForm(),
+    );
+  }
+}
---
+++ lib/sign_up.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class MyForm extends StatelessWidget {
+  MyForm({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {}
+  }
+
+  String? _validateEmail(value){
+    if(value!.isEmpty) {
+      return 'Please enter an email';
+    }
+    RegExp emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegExp.hasMatch(value)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _validatePhoneNumber(value) {
+    if (value!.isEmpty) {
+      return 'Please enter a phone number';
+    }
+    if (value.length != 11) {
+      return 'Please enter a 11-digit phone number';
+    }
+    return null;
+  }
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My Form'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+              child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                    labelText: 'Username',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Please enter a username';
+                      }
+                      return null;
+                    },
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                    validator: (_validateEmail),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: InputDecoration(
+                    labelText: 'Phone Number',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0))),
+                    validator: _validatePhoneNumber,
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              Container(
+                height: 50,
+                width: double.infinity,
+                  child:
+                      ElevatedButton(onPressed: _submitForm, child: Text("Submit"))),
+            ],
+          )),
+        ));
+  }
+}
